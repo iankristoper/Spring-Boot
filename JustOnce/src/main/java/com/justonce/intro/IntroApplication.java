67e1e00d35d9850038d79d513ea90d570a11dc33
iankristoper <iankristoper26@gmail.com>
2025-03-30 00:07:36 +0800

    minor changes and corrections of the code

---
+++ JustOnce/src/main/java/com/justonce/intro/IntroApplication.java
@@ -0,0 +1,60 @@
+package com.justonce.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class IntroApplication {
+    public static void main(String[] args) {
+        System.out.println("Starting spring boot application...123");
+
+        //create the application context container -> will handle beans and load all the beans 
+        ApplicationContext context = SpringApplication.run(IntroApplication.class, args);
+        System.out.println("Application context loaded......");
+        
+        //retrieving the beans from the context container
+        //Car car = context.getBean(Car.class);
+        //car.start();
+    }
+    
+    @Component
+    class Car {
+        private final Engine engine;
+        
+        //constructor-based dependency injection
+        public Car(Engine engine) {
+            this.engine = engine;
+            System.out.println("Car has been created...by the context container");
+            System.out.println("The constructor has been initialized with dependency injection");
+        }
+        
+        public void start() {
+            System.out.println("Car is starting...vroom vrooom! -> coming from the start function");
+            engine.run();
+        }
+    }
+  
+    @Component 
+    class Engine {
+        public Engine() {
+            System.out.println("Engine been created...by the context container");
+        }
+        
+        public void run() {
+            System.out.println("Engine is running...chrrr -> from the run function");
+        }
+    }
+    
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> System.out.println("Application Running...this is from the commanline runner");
+    }
+}
--- hello/src/test/java/com/hello/simple/Hello.java
+++ hello/src/test/java/com/hello/simple/Hello.java
@@ -1,14 +1,11 @@
 package com.hello.simple;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import jakarta.annotation.PostConstruct;
 
 
 @SpringBootApplication
@@ -18,11 +15,10 @@ public class Hello {
         
         // Using ApplicationContext to manage beans
         ApplicationContext context = SpringApplication.run(Hello.class, args);
-        System.out.println("Application context loaded...");
+        System.out.println("Application context loaded......");
         
         // Fetching a bean from the context container
         MyBean sampleBean = context.getBean(MyBean.class);
-        sampleBean.sayHello();
     }
     
     @Component
@@ -30,20 +26,8 @@ public class Hello {
         public MyBean() {
             System.out.println("MyBean has been initialized...");
         }
-        
-        public void sayHello() {
-            System.out.println("Hello from sayHello -> MyBean");
-        }
-        
-        @PostConstruct
-        public void init() {
-            System.out.println("MyBean PostConstruct has been executed");
-        }
     }
     
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> System.out.println("\n🚀 Application Running...");
-    }
+    
     
 }
