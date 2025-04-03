721c4cbbb43b5107667496ba4a2b6a1e15ee5f8b
iankristoper <iankristoper26@gmail.com>
2025-04-04 02:06:57 +0800

    another changes

--- MyAOP/src/main/java/com/testing/aop/AopApplication.java
+++ MyAOP/src/main/java/com/testing/aop/AopApplication.java
@@ -3,25 +3,53 @@ package com.testing.aop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.stereotype.Service;
-import org.
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.After;
+
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 
 
 
 
 @SpringBootApplication
+@EnableAspectJAutoProxy // Enables AspectJ-based AOP in Spring
 public class AopApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AopApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AopApplication.class, args);
+        
+        // Retrieve the service bean and call its method
+        MyService myService = context.getBean(MyService.class);
+        myService.doSomething();
     }
 }
 
-//
+//Business logic is in here (service layer)
 @Service
 class MyService {
     public void doSomething() {
         System.out.println("This is coming from the service annotation");
+    }
+}
+
+//AOP for logging 
+@Aspect
+@Component
+class LoggingAspect {
+    //log before method execution 
+    @Before("execution(* MyService.*(..))")
+    public void logBeforeMethod() {
+        System.out.println("AOP Logging: Before main method");
+    }
+    
+    //Log after method execution
+    @After("execution(* MyService.*(..))")
+    public void logAfterMethod() {
+        System.out.println("AOP Logging: After main method");
     }
 }
