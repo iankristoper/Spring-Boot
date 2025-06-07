39717754f606607a9d2ed5bebdd1be172b303814
iankristoper <iankristoper26@gmail.com>
2025-06-07 17:07:25 +0800

    minor changes

--- Attendance Tracker/src/main/java/dao/UserRepository.java
+++ Attendance Tracker/src/main/java/dao/UserRepository.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+
 package dao;
+
+
 
 /**
  *
- * @author Ian Tagano
+ * @author noob
  */
+
+
+import model.User;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+
+
+
+    
+
+@Repository
 public class UserRepository {
     
+    private final JdbcTemplate jdbc;   
+    
+    public UserRepository(JdbcTemplate jdbc) { //contructor based dependency injection - no need autowired it is automatic
+        this.jdbc = jdbc;
+    }
+    
+    
+    //create -> insert data in the database
+    public int createUser(User user) {
+        String sql = "INSERT INTO employee(name) VALUES=?";
+        return jdbc.update(sql, user.getName());
+    }
+    
+    
+    //read -> read data on the database - by id only
+    public User getUserId(int id) {
+        String sql = "SELECT * FROM employee WHERE id=?";
+        return jdbc.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), id);
+    }
+    
+    
+    //read -> raad data from the database - all data
+    public List<User> getAllUser() {
+        String sql = "SELECT * FROM employee";
+        return jdbc.query(sql, new BeanPropertyRowMapper<>(User.class));
+    }
+    
+    
+    //delete -> delete data from the database
+    public int deleteUser(int id) {
+        String sql = "DELETE FROM employee WHERE id=?";
+        return jdbc.update(sql, id);
+    } 
 }
--- Attendance Tracker/src/main/java/model/User.java
+++ Attendance Tracker/src/main/java/model/User.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
 /**
  *
- * @author Ian Tagano
+ * @author noob
  */
+
+
+
+
 public class User {
     
+    private int id;
+    private String name;
+    
+    public User() {
+        
+    }
+       
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    
+    //getters and setter
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
 }
